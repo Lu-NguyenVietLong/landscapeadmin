b3e9b0fcec556aa1dd3eec247794cdee3bd73703
longorlu <longorlu2001@gmail.com>
2024-05-16 12:58:13 +0700

    fix tree interface

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,28 +1,23 @@
-import { getAllTree } from '@/packages/services/treesApi'
-import { map } from 'lodash'
-import React from 'react'
+import { getAllTree } from "@/packages/services/treesApi";
+import { map } from "lodash";
+import React from "react";
 
 const page = async () => {
-
-    const tree = await getAllTree()
-    console.log("Tree->", tree.trees)
+  const tree = await getAllTree();
+  //   console.log("Tree->", tree);
 
   return (
     <>
-        <h1>
-            Cây
-        </h1>
-        <div>
-            <ul>
-                {map(tree.trees, (tree) => (
-                    <li key={tree._id}>
-                        {tree.name}
-                    </li>
-                ))}
-            </ul>
-        </div>
+      <h1>Cây</h1>
+      <div>
+        <ul>
+          {/* {map(tree.trees, (tree) => (
+            <li key={tree._id}>{tree.name}</li>
+          ))} */}
+        </ul>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default page
+export default page;
---
+++ src/packages/interfaces/tree.ts
@@ -0,0 +1,7 @@
+interface ITree {
+    _id: string,
+    name: string,
+    slug: string,
+    description: string,
+    isContacPrice: true
+}
