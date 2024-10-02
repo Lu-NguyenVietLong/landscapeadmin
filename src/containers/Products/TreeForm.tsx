7012ac550c183a08a836b5c472c29b5e03c46a12
longorlu <longorlu2001@gmail.com>
2024-10-02 21:19:05 +0700

    kk

--- src/containers/Products/TreeForm.tsx
+++ src/containers/Products/TreeForm.tsx
@@ -190,7 +190,7 @@ const TreeForm = ({ type, onClose, tree, onSuccess }: ITreeFormProp) => {
   const handleChange: UploadProps["onChange"] = ({ fileList: newFileList }) => {
     let validFiles = newFileList.filter((file: any) => {
       if (file.originFileObj) {
-        return ["image/jpeg", "image/png", "image/jpg", "image/heìf"].includes(
+        return ["image/jpeg", "image/png", "image/jpg", "image/heif"].includes(
           file?.type || ""
         );
       } else {
